7220ea9ef5b8d2b43ab319c97d8bbd9e8623e8d5
byMagg <byMag@hotmail.es>
2023-03-02 17:54:07 +0100

    update paths

--- activity-1/capacitor.config.ts
+++ activity-1/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'navApp',
+  appName: 'activity-1',
   webDir: 'www',
   bundledWebRuntime: false
 };
