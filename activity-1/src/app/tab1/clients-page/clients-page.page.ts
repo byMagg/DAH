b7e0932cdac5b151db2d425d52e396e0730d0e9d
byMagg <byMag@hotmail.es>
2023-03-03 13:05:32 +0100

    add list every page

--- activity-1/src/app/tab1/clients-page/clients-page.page.ts
+++ activity-1/src/app/tab1/clients-page/clients-page.page.ts
@@ -9,6 +9,10 @@ import { NavController } from '@ionic/angular';
 export class ClientsPagePage implements OnInit {
 
   buttonPath = "/tabs/tab1/clients-page/edit-client-page"
+  elements: any[] = [
+    { "id": "EL1", "name": "Element 1", "description": "Desc. of EL1" },
+    { "id": "EL2", "name": "Element 2", "description": "Desc. of EL2" }
+  ]
 
   constructor(public navCtrl: NavController) { }
 
--- activity-1/src/app/tab2/reservations-page/reservations-page.page.ts
+++ activity-1/src/app/tab2/reservations-page/reservations-page.page.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationsPagePage implements OnInit {
 
+  elements: any[] = [
+    { "id": "EL1", "name": "Element 1", "description": "Desc. of EL1" },
+    { "id": "EL2", "name": "Element 2", "description": "Desc. of EL2" }
+  ]
+
   constructor() { }
 
   ngOnInit() {
