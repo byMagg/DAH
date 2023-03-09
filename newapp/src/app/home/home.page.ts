e7931cb89aaeef777a2ebf8dd89ef5236c1d6ee5
byMagg <byMag@hotmail.es>
2023-03-09 17:28:25 +0100

    fix results

--- newapp/src/app/home/home.page.ts
+++ newapp/src/app/home/home.page.ts
@@ -14,7 +14,7 @@ export class HomePage implements OnInit {
   constructor(private newService: NewserviceService) { }
 
   ngOnInit() {
-    this.newService.searchMovie("Ant Man");
+    this.results = this.newService.searchMovie("Ant Man");
   }
 
 }
