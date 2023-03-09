725fd7c545a626f211b6cbc6ba9bcfcc890301f7
byMagg <byMag@hotmail.es>
2023-03-09 17:17:22 +0100

    test

---
+++ newapp/src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NewserviceService } from '../services/newservice.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  results: Observable<any> | undefined;
+
+  constructor(private newService: NewserviceService) { }
+
+  ngOnInit() {
+    this.newService.searchMovie("Ant Man");
+  }
+
+}
