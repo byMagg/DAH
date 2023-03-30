bbfb8c39acc23357ad11b0e4607ea8f1a286f2bd
byMagg <byMag@hotmail.es>
2023-03-30 18:09:54 +0200

    fix implementation with ionic 7

--- activity-5/src/app/app.routes.ts
+++ activity-5/src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'new-page/:word',
+    loadComponent: () => import('./new-page/new-page.page').then(m => m.NewPagePage)
+  },
 ];
--- activity-5/src/app/home/home.page.ts
+++ activity-5/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { GenerateTextService } from '../services/generate-text.service';
 
@@ -8,14 +9,14 @@ import { GenerateTextService } from '../services/generate-text.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, FormsModule]
+  imports: [IonicModule, FormsModule, RouterModule]
 })
 export class HomePage {
 
   public title: string = "Home Page";
   public word: string = "initial word";
 
-  constructor(public testservice: GenerateTextService) { }
+  constructor(public testservice: GenerateTextService, public route: ActivatedRoute) { }
 
   getWord() {
     this.word = this.testservice.getRandomWord();
@@ -23,6 +24,10 @@ export class HomePage {
 
   changeTitle(title: string) {
     this.title = title;
+  }
+
+  navigateToNewPage() {
+    // this.route.
   }
 
 }
