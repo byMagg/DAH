5b10f8b74d8672a45637a6f1f2f9035fa26e7cb9
byMagg <byMag@hotmail.es>
2023-03-31 12:39:53 +0200

    fix test editclienpage

--- activity-5/src/app/tab1/clients-page/edit-client-page/edit-client-page.page.spec.ts
+++ activity-5/src/app/tab1/clients-page/edit-client-page/edit-client-page.page.spec.ts
@@ -1,11 +1,17 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { EditClientPagePage } from './edit-client-page.page';
+import { IonicModule } from '@ionic/angular';
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('EditClientPagePage', () => {
   let component: EditClientPagePage;
   let fixture: ComponentFixture<EditClientPagePage>;
 
   beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(), RouterTestingModule]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(EditClientPagePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
