0e2caedf8826f0e565757e93e0f5746d55a2cc07
byMagg <byMag@hotmail.es>
2023-04-02 16:56:40 +0200

    add routing test

--- activity-5/cypress/e2e/test.spec.cy.ts
+++ activity-5/cypress/e2e/test.spec.cy.ts
@@ -1,5 +1,23 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('routing clients', () => {
     cy.visit('/')
+    cy.get('#tab-button-tab1').click()
+    cy.get('app-tab1 ion-button').click()
+    cy.get('ion-back-button').click()
   })
+
+  it('routing reservations', () => {
+    cy.visit('/tabs/tab1')
+    cy.get('#tab-button-tab2').click()
+    cy.get('app-tab2 ion-button').click()
+    cy.get('ion-back-button').click()
+  })
+
+  it('routing products', () => {
+    cy.visit('/')
+    cy.get('#tab-button-tab3').click()
+    cy.get('app-tab3 ion-button').click()
+    cy.get('ion-back-button').click()
+  })
+
 })
